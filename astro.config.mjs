2cce1036520efe032431ff94519aeca8781ddd91
Ishan Bagchi <ishanbagchi4@gmail.com>
2026-02-22 18:31:51 +0530

    feat: update site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ishanbagchi.com',
+	site: 'https://logs.ishanbagchi.com',
 	markdown: {
 		shikiConfig: {
 			themes: {
