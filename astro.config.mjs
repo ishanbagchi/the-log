b17f4854630cb735266aad13e536fbb9b4656af1
Ishan Bagchi <ishanbagchi4@gmail.com>
2026-02-22 18:17:59 +0530

    feat: refactor blog routing and update canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/blog',
 	site: 'https://ishanbagchi.com',
 	markdown: {
 		shikiConfig: {
