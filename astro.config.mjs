61507efed06423e648cd2fa5902b476096e9a24f
Ishan Bagchi <ishanbagchi4@gmail.com>
2026-02-22 16:06:45 +0530

    feat: set base path for blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/blog',
 	site: 'https://ishanbagchi.com',
 	markdown: {
 		shikiConfig: {
